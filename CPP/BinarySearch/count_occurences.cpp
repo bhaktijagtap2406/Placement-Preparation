bf4ff0aa5585e3d836202f37807f2e8acec36254
bhaktijagtap2406 <bhaktijagtap2406@gmail.com>
2026-07-11 20:36:41 +0530

    Add Last Occurence implementation

--- CPP/BinarySearch/count_occurences.cpp
+++ CPP/BinarySearch/count_occurences.cpp
@@ -13,11 +13,10 @@ int main(){
     while(start <= end){
         int mid = start + (end - start) / 2;
 
-        //for last occurence
+        //for first occurence
         if(arr[mid] == target){
             ans = mid;
             start = mid + 1;
-
         }
         else if(arr[mid] < target){
             start = mid + 1;
