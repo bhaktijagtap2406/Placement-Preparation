507d0e43c57ac4f93f0fdc0c7c9ac8ccab1a9d14
bhaktijagtap2406 <bhaktijagtap2406@gmail.com>
2026-07-12 18:25:19 +0530

    Add

---
+++ Python/Day3/Calculator.py
@@ -0,0 +1,9 @@
+def multiply(x, y):
+    return x * y
+
+
+def add(x,y):
+    return x + y 
+
+def subtract(x,y):
+    return x - y
---
+++ Python/Day3/main.py
@@ -0,0 +1,18 @@
+# import Calculator
+
+# print(Calculator.multiply(2,5))
+
+
+# from Calculator import multiply,add,subtract
+
+# print(multiply(2,4))
+# print(add(2,4))
+# print(subtract(2,4))
+
+
+
+import Calculator as cal #Alias
+
+print(cal.multiply(2,5))
+print(cal.add(2,5))
+print(cal.subtract(2,5))
