830088787f22afffc1988dfbdaaa95c684726797
bhaktijagtap2406 <bhaktijagtap2406@gmail.com>
2026-07-22 20:09:39 +0530

    Add

---
+++ LeetCode/Day10/682_Baseball_Game.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> st;
+        int sum = 0;
+        for(int i = 0;i<operations.size();i++){
+            if(operations[i] != "C" && operations[i] != "D" && operations[i] != "+"){
+                st.push(stoi(operations[i]));
+            }
+            else if(operations[i] == "D"){
+                st.push(st.top()*2);
+            }
+            else if(operations[i] == "C"){
+                st.pop();
+            }
+            else if(operations[i] == "+"){
+                int first = st.top();
+                st.pop();
+                int second = st.top();
+                st.push(first);
+                st.push(first + second);
+            }
+        }
+        while(!st.empty()){
+            sum += st.top();
+            st.pop();
+        }
+        return sum;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<string> operations = {"5","2","C","D","+"};
+    cout<<s.calPoints(operations);
+}
