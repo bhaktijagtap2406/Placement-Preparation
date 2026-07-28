8c13c0b2cb32ef6660c15e20f4d92da8c758fd65
bhaktijagtap2406 <bhaktijagtap2406@gmail.com>
2026-07-28 21:45:46 +0530

    Add

--- SQL/Day16/joins_union.sql
+++ SQL/Day16/joins_union.sql
@@ -20,3 +20,58 @@ UNION
 
 SELECT Department
 FROM Department;
+
+SELECT *
+FROM Employees
+WHERE salary > (
+    SELECT AVG(salary)
+    FROM Employees
+);
+
+SELECT name
+FROM Employees
+WHERE department_id = (
+    SELECT department_id
+    FROM Departments
+    WHERE department_name = 'Engineering'
+);
+
+SELECT name
+FROM Employees
+WHERE department_id IN (
+    SELECT department_id
+    FROM Projects
+);
+
+SELECT name, salary
+FROM Employees e
+WHERE salary > (
+    SELECT AVG(salary)
+    FROM Employees
+    WHERE department_id = e.department_id
+);
+
+SELECT department_name
+FROM Departments d
+WHERE EXISTS (
+    SELECT *
+    FROM Employees e
+    WHERE e.department_id = d.department_id
+);
+
+SELECT
+    name,
+    (
+        SELECT department_name
+        FROM Departments d
+        WHERE d.department_id = e.department_id
+    ) AS department
+FROM Employees e;
+
+SELECT AVG(salary)
+FROM (
+    SELECT salary
+    FROM Employees
+    WHERE salary > 50000
+) AS high_salary;
+
