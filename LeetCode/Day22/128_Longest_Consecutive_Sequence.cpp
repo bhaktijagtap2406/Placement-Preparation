22b1a8d1a2c9b6b093e3b41cd88a2e043a87afba
bhaktijagtap2406 <bhaktijagtap2406@gmail.com>
2026-08-05 22:45:15 +0530

    Add

--- LeetCode/Day22/128_Longest_Consecutive_Sequence.cpp
+++ LeetCode/Day22/128_Longest_Consecutive_Sequence.cpp
@@ -26,3 +26,13 @@ public:
         return count;
     }
 };
+
+int main() {
+    Solution solution;
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
+    int result = solution.longestConsecutive(nums);
+    
+    cout << "Length of the longest consecutive sequence: " << result << endl;
+
+    return 0;
+}
