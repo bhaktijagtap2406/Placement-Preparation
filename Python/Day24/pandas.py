c708b2b96c4226549eec39f50333fffc110ce3e3
bhaktijagtap2406 <bhaktijagtap2406@gmail.com>
2026-08-09 00:41:33 +0530

    Add

---
+++ Python/Day24/pandas.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.DataFrame({
+    "Name": ["A", "B", "C"],
+    "Age": [20, 21, 22],
+    "Salary": [40000, 50000, 60000]
+})
+
+df.loc[0, "Name"]
+
+df.iloc[0, 0]
+
+df.loc[:, ["Name", "Salary"]]
+df.iloc[:, [0, 2]]
+
+df[df["Salary"] > 45000]
+
+df[(df["Age"] > 20) & (df["Salary"] > 45000)]
+df["Salary"].isnull()
+
+df["Salary"].fillna(0)
+
+df.dropna()
